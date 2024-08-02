bfcee07eece6b2bd951f29b515af3b80814c75a7
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-08-02 17:48:17 +0300

    Базовый класс для команды

---
+++ Infrastructure/Commands/Base/BaseCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace StaticRustLauncher.Infrastructure.Commands.Base;
+
+public abstract class BaseCommand : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public event EventHandler? CommandExecuted 
+    { 
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public abstract bool CanExecute(object? parameter);
+
+    public abstract void Execute(object? parameter);    
+}
