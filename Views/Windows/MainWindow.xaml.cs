f4bf6ed2e93571d46d7ca3e8f35b7aad33824da6
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-08-02 17:25:16 +0300

    Изменил директория и Namespace для главного окна

---
+++ Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace StaticRustLauncher.Views.Windows;
+
+/// <summary>
+/// Главное окно приложения
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
