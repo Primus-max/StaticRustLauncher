451b80b4679c9e99d39149b7477c58d046c25ea4
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-08-03 20:10:39 +0300

    Модальное окно играть

---
+++ ViewModels/PlayNowViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace StaticRustLauncher.ViewModels;
+
+class PlayNowViewModel
+{
+    public ICommand PlayNowCommand { get; set; } = null!;
+
+    public PlayNowViewModel()
+    {
+        PlayNowCommand = new LambdaCommand(OnPlayNowExecuted);
+    }
+
+    private void OnPlayNowExecuted(object parameter)
+    {
+        PlayNowWindow playNowWindow = new();
+        var mainWindow = Application.Current.MainWindow;
+        playNowWindow.Owner = mainWindow;
+
+        BlurEffectHelper.ApplyBlurEffect(mainWindow, 0, 10, 0.5);
+        playNowWindow.ShowDialog();
+        BlurEffectHelper.RemoveBlurEffect(mainWindow, 10, 0, 0.5);
+    }
+}
---
+++ Views/Windows/PlayNowWindow.cs
@@ -0,0 +1,13 @@
+﻿namespace StaticRustLauncher.Views.Windows;
+
+/// <summary>
+/// Логика взаимодействия для PlayNowWindow
+/// </summary>
+public partial class PlayNowWindow : Window
+{
+    public PlayNowWindow()
+    {
+        InitializeComponent();
+        DataContext = new PlayNowViewModel();
+    }
+}
