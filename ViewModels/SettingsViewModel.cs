76178be683284172bf20490e0f08aebc3845c662
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-08-06 07:04:24 +0300

    логика выбора файлов

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,6 +1,47 @@
-﻿namespace StaticRustLauncher.ViewModels;
+﻿
+using Microsoft.Win32;
+
+namespace StaticRustLauncher.ViewModels;
 
 public class SettingsViewModel : BaseViewModel
 {
+    public ICommand? OpenFileDialogCommand { get; }
+
+    private string? _launcherFilePath;
+    private string? _gameFilePath;
+    public string? LauncherFilePath 
+    { 
+        get => _launcherFilePath; 
+        set => Set(ref _launcherFilePath, value); 
+    }
+    public string? GameFilePath 
+    { 
+        get => _gameFilePath; 
+        set => Set(ref _gameFilePath, value); 
+    }
+
+    public SettingsViewModel()
+    {
+        OpenFileDialogCommand = new LambdaCommand(OnOpenFileDialog);        
+    }
+
+    private void OnOpenFileDialog(object commandName)
+    {
+        OpenFileDialog openFile = new();
+        if (openFile is null || openFile.ShowDialog() != true) return;
+
+        switch (commandName)
+        {
+            case "Launcher":
+                LauncherFilePath = openFile.FileName;
+                break;
+            case "Game":
+                GameFilePath = openFile.FileName;
+                break;
+            default:
+                break;
+        }
+
+    }
 
 }
